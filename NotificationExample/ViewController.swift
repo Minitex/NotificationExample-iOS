9b56d96f2046a4d18d22d4b543328ceec53ef75e
Vui Nguyen <vui_engineer@yahoo.com>
2018-09-13 15:28:10 -0600

    initial local notification

--- NotificationExample/ViewController.swift
+++ NotificationExample/ViewController.swift
@@ -7,11 +7,33 @@
 //
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
   @IBAction func sendNotification(_ sender: Any) {
     print("send local notification")
+
+    //creating the notification content
+    let content = UNMutableNotificationContent()
+
+    //adding title, subtitle, body and badge
+    content.title = "Book Ready to Check Out"
+    content.subtitle = "Book Title Ready to Check Out"
+    content.body = "You'll Have the Option to Check Out Book from Here Later"
+    content.badge = 1
+
+    //getting the notification trigger
+    //it will be called after 2 seconds
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
+
+    //getting the notification request
+    let request = UNNotificationRequest(identifier: "SimplyEIOSNotification", content: content, trigger: trigger)
+
+    UNUserNotificationCenter.current().delegate = self
+
+    //adding the notification to notification center
+    UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
   }
   
   @IBAction func startTimer(_ sender: Any) {
@@ -19,9 +41,18 @@ class ViewController: UIViewController {
   }
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    // request for user authorization
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: {didAllow, error in
+
+    })
   }
 
+  func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+
+    //displaying the ios local notification when app is in foreground
+    completionHandler([.alert, .badge, .sound])
+  }
 
 }
 
