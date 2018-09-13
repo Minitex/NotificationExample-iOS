95ac440a34e6260778123f19251f42734f98f3bf
Vui Nguyen <vui_engineer@yahoo.com>
2018-09-13 10:52:44 -0600

    initial commit - add button

---
+++ NotificationExample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NotificationExample
+//
+//  Created by Vui Nguyen on 9/12/18.
+//  Copyright © 2018 Minitex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBAction func sendNotification(_ sender: Any) {
+    print("send local notification")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
