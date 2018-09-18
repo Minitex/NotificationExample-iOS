087d66129d1c57fdd64935c7f1586b41ea198325
Vui Nguyen <vui_engineer@yahoo.com>
2018-09-18 14:32:11 -0600

    got onscreen timer working

--- NotificationExample/ViewController.swift
+++ NotificationExample/ViewController.swift
@@ -12,13 +12,13 @@ import UserNotifications
 class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
   enum NotificationTimes: Int {
-    case none = 0, five, ten, fifteen
+    case none = 0, half, one, five, ten, fifteen
   }
 
   var timer = Timer()
   var timerIsOn = false
-  var timeRemaining: Int = 0
-  var timeSelected: Int = 0
+  var timeRemaining: Double = 0
+  var timeSelected: Double = 0
 
   // MARK: Outlets
   @IBOutlet weak var timerSegmentControl: UISegmentedControl!
@@ -26,11 +26,27 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
   @IBOutlet weak var timerLabel: UILabel!
 
   // MARK: Actions
+  @IBAction func printPendingNotifications(_ sender: Any) {
+    UNUserNotificationCenter.current().getPendingNotificationRequests(completionHandler: { requests in
+      print("About to print some pending notifications, yo!")
+      for request in requests {
+        print(request)
+      }
+    })
+  }
+
+
   @IBAction func timeSelected(_ sender: Any) {
-    var minutes = 0
+    var minutes: Double = 0
     switch(timerSegmentControl.selectedSegmentIndex) {
     case NotificationTimes.none.rawValue:
       print("none selected")
+    case NotificationTimes.half.rawValue:
+      print("half minute selected")
+      minutes = 0.5
+    case NotificationTimes.one.rawValue:
+      print("one selected")
+      minutes = 1
     case NotificationTimes.five.rawValue:
       print("five selected")
       minutes = 5
@@ -46,37 +62,16 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
     // totalTime in seconds
     timeSelected = 60 * minutes
-    calculateTimer()
+    displayTimerRunning(timeSelected)
   }
 
 
   @IBAction func sendNotification(_ sender: Any) {
     print("send local notification")
-
-    //creating the notification content
-    let content = UNMutableNotificationContent()
-
-    //adding title, subtitle, body and badge
-    content.title = "Book Ready to Check Out"
-    content.subtitle = "Book Title Ready to Check Out"
-    content.body = "You'll Have the Option to Check Out Book from Here Later"
-    content.badge = 1
-
-    //getting the notification trigger
-    //it will be called after 3 seconds
-    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
-
-    //getting the notification request
-    let request = UNNotificationRequest(identifier: "SimplyEIOSNotification", content: content, trigger: trigger)
-
-    UNUserNotificationCenter.current().delegate = self
-
-    //adding the notification to notification center
-    // for the completion handler, this might be where we start the timer on the UI
-    UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    createNotification()
   }
   
-  @IBAction func startTimer(_ sender: Any) {
+  @IBAction func startStopTimer(_ sender: Any) {
 
     // this is where we start the timer going and submit the notification request
     // allow the timer to continue when app is backgrounded as well
@@ -87,13 +82,21 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
       for state: UIControl.State in [.normal, .highlighted, .disabled, .selected, .focused, .application, .reserved] {
         timerButton.setTitle("Start Timer", for: state)
       }
+      timer.invalidate()
       timerIsOn = false
       print("stopped the notification timer")
     } else {
+      if timeSelected == 0 { return }
       timerSegmentControl.isEnabled = false
       for state: UIControl.State in [.normal, .highlighted, .disabled, .selected, .focused, .application, .reserved] {
         timerButton.setTitle("Stop Timer", for: state)
       }
+
+      timeRemaining = timeSelected
+
+      // it does not make sense to pass the timer code as the completion handler for the notification
+      createNotification(timeSeconds: Int(timeRemaining), repeat: false, completionHandler: nil)
+      timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerRunning), userInfo: nil, repeats: true)
       timerIsOn = true
       print("started the notification timer")
     }
@@ -123,14 +126,41 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
   }
 
   // MARK: helper functions
-  func calculateTimer() -> Void {
-    let minutesLeft = Int(timeSelected) / 60 % 60
-    let secondsLeft = Int(timeSelected) % 60
+  func displayTimerRunning(_ remainingTime: Double) -> Void {
+    let minutesLeft = Int(remainingTime) / 60 % 60
+    let secondsLeft = Int(remainingTime) % 60
     timerLabel.text = "\(minutesLeft):\(secondsLeft)"
   }
 
-  func manageTimerEnd(seconds: Int) -> Void {
+  @objc func timerRunning() -> Void {
+    timeRemaining -= 1
+    displayTimerRunning(timeRemaining)
+    if timeRemaining == 0 {
+      timerIsOn = true
+      startStopTimer(self)
+    }
+  }
 
+  func createNotification(timeSeconds: Int = 3, repeat: Bool = false, completionHandler: (() -> Void)? = nil) -> Void {
+    let content = UNMutableNotificationContent()
+
+    //adding title, subtitle, body and badge
+    content.title = "Book Ready to Check Out"
+    content.subtitle = "Book Title Ready to Check Out"
+    content.body = "You'll Have the Option to Check Out Book from Here Later"
+    content.badge = 1
+
+    //getting the notification trigger
+    //it will be called after X seconds
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(timeSeconds), repeats: false)
+
+    //getting the notification request
+    let request = UNNotificationRequest(identifier: "SimplyEIOSNotification", content: content, trigger: trigger)
+
+    UNUserNotificationCenter.current().delegate = self
+
+    //adding the notification to notification center
+    UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
   }
 }
 
